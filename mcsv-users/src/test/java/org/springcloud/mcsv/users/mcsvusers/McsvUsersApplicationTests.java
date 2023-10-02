38a30f0fa3f2e4e2c580569dc821235c0abf9ed8
Daniel-LA0303 <03daniel030399@gmail.com>
2023-10-02 09:46:50 -0600

    add users service

---
+++ mcsv-users/src/test/java/org/springcloud/mcsv/users/mcsvusers/McsvUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springcloud.mcsv.users.mcsvusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsvUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
