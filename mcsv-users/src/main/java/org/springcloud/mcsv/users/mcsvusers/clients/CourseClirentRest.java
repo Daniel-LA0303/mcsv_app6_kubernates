30e3ebc5be4acfeb031624755e5ba96f77f18625
Daniel-LA0303 <03daniel030399@gmail.com>
2023-10-10 18:12:05 -0600

    change uri in feing clients

--- mcsv-users/src/main/java/org/springcloud/mcsv/users/mcsvusers/clients/CourseClirentRest.java
+++ mcsv-users/src/main/java/org/springcloud/mcsv/users/mcsvusers/clients/CourseClirentRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "mcsv-cours", url = "mcsv-cours:8082")
+@FeignClient(name = "mcsv-cours", url = "http://cours-mcsv-container:8082")
 public interface CourseClirentRest {
 
     @DeleteMapping("/delete-user-by-id/{userId}")
